d9fc054ea112c4129d66d36c3fd6a2ddd36b6974
krithickthangaraj <124655718+krithickthangaraj@users.noreply.github.com>
2025-01-03 13:52:11 +0530

    Time: 0 ms (100%), Space: 41.3 MB (34.63%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -1,52 +1,9 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-    List<Integer> res1 = new ArrayList<>();
-    List<Integer> res2 = new ArrayList<>();
+        if (p == null && q == null) return true; 
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false; 
 
-    res1=trav(p);
-    res2=trav(q);
-    if(res1.equals(res2))
-    {
-        return true;
-    }
-    return false;
-
-        
-    }
-    public List<Integer> trav(TreeNode node )
-    {
-            List<Integer> res = new ArrayList<>();
-
-        Stack<TreeNode> st=new Stack<>();
-        TreeNode curr=node;
-        while(curr!=null || !(st.isEmpty()))
-        {
-            while(curr!=null)
-            {
-                st.push(curr);
-                curr=curr.left;
-            }
-            curr=st.pop();
-            res.add(curr.val);
-            curr=curr.right;
-        }
-        return res;
-
-
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }

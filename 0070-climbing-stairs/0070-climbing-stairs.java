7ced1799cc92eb39e4513d2de4e6b57610cd5552
krithickthangaraj <124655718+krithickthangaraj@users.noreply.github.com>
2025-01-21 14:15:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solve(int n,int[] a){
+        if(n==2 || n==1) return n;
+        if(a[n]!=-1) return a[n];
+        return solve(n-1,a)+solve(n-2,a);
+
+    }
+    public int climbStairs(int n) {
+        int a[]=new int[n+1];
+        Arrays.fill(a,-1);
+        return solve(n,a);
+        
+    }
+}

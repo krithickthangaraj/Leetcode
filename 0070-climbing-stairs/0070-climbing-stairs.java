7036864e8610049529abe4fc4fec9893ad781695
krithickthangaraj <124655718+krithickthangaraj@users.noreply.github.com>
2025-01-21 14:17:47 +0530

    Time: 0 ms (100%), Space: 40.3 MB (63.86%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,7 +2,7 @@ class Solution {
     public int solve(int n,int[] a){
         if(n==2 || n==1) return n;
         if(a[n]!=-1) return a[n];
-        return solve(n-1,a)+solve(n-2,a);
+        return a[n]=solve(n-1,a)+solve(n-2,a);
 
     }
     public int climbStairs(int n) {

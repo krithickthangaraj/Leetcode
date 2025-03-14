7fb0ecd49f966caf8c3a8a0a8ec320f81e0742b9
KRITHICK THANGARAJ <krithick18112004@gmail.com>
2025-03-14 20:55:23 +0530

    Time: 51 ms (47.56%), Space: 55.2 MB (52.44%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.java
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -6,7 +6,7 @@ class Solution {
         while(r<arr.length)
         {
             in.put(arr[r],in.getOrDefault(arr[r],0)+1);
-            while(in.size()>=3)
+            if(in.size()>=3)
             {
                 in.put(arr[l],in.get(arr[l])-1);
                 if(in.get(arr[l])==0) in.remove(arr[l]);

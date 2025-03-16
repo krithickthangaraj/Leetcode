fa409ba7000a602b90318f3d3f132cf634b54640
KRITHICK THANGARAJ <krithick18112004@gmail.com>
2025-03-16 19:19:49 +0530

    Time: 54 ms (51.61%), Space: 56.5 MB (38.98%) - LeetHub

--- 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.java
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.java
@@ -1,46 +1,24 @@
 class Solution {
-    public long repairCars(int[] arr, int c) {
-        long[] ranks = new long[arr.length];
-        int idx = 0;
-        for(int ele : arr)
-        {
-            ranks[idx++] = (ele * 1l);
+    public boolean timeIsSuff(int[] ranks, int cars, long minGiven) {
+        long carsDone = 0;
+        for (int r : ranks) {
+            long c2 = minGiven / r;
+            long c = (long) Math.sqrt(c2);
+            carsDone += c;
         }
-        Arrays.sort(ranks);
-        long cars = (c * 1l);
-        
-        int n = ranks.length;
-        long time = (int)1e14;
-        long si = 0;
-        long ei = Long.MAX_VALUE;
-
-        while(si <= ei)
-        {
-            long mid = (si) + (ei - si)/2;
-            if(helper(mid, ranks, cars))
-            {
-                time = mid;
-                ei = mid - 1;
-            }
-            else
-            {
-                si = mid + 1;
-            }
-        }
-        return time; 
+        return carsDone >= cars;
     }
-    
-    public boolean helper(long mid, long[] ranks, long cars)
-    {
-        for(int i = 0; i < ranks.length; i++)
-        {
-            long x = (long)Math.floor(Math.sqrt((mid * 1.0)/(ranks[i] * 1.0)));
-            if(x >= cars)
-                return true;
-            cars-= x;
-            if(cars == 0)
-                return true;
+
+    public long repairCars(int[] ranks, int cars) {
+        long l = 1, r = (long) 1e14;
+        while (l < r) {
+            long mid = (l + r) / 2;
+            if (timeIsSuff(ranks, cars, mid)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
         }
-        return (cars == 0);
+        return l;
     }
 }

224d67062a62e5877953631d5b85dfe614b40f70
KRITHICK THANGARAJ <krithick18112004@gmail.com>
2025-05-02 04:28:35 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0838-push-dominoes/0838-push-dominoes.java
+++ 0838-push-dominoes/0838-push-dominoes.java
@@ -1,29 +1,37 @@
 class Solution {
     public String pushDominoes(String s) {
-        s = "L" + s + "R";
-        StringBuilder res = new StringBuilder();
-        int prev = 0;
-        for (int curr = 1; curr < s.length(); ++curr) {
-            if (s.charAt(curr) == '.') continue;
-            int span = curr - prev - 1;
-            if (prev > 0)
-                res.append(s.charAt(prev));
-            if (s.charAt(prev) == s.charAt(curr)) {
-                for (int i = 0; i < span; ++i)
-                    res.append(s.charAt(prev));
-            } else if (s.charAt(prev) == 'L' && s.charAt(curr) == 'R') {
-                for (int i = 0; i < span; ++i)
-                    res.append('.');
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        int left = 0;
+
+        char[] result = new char[n];
+        for (int i = 0; i < n; i++) result[i] = arr[i];
+
+        int i = 0;
+        while (i < n) {
+            if (arr[i] == '.') {
+                int j = i;
+                while (j < n && arr[j] == '.') j++;
+
+                char leftChar = (i == 0) ? 'L' : arr[i - 1];
+                char rightChar = (j == n) ? 'R' : arr[j];
+
+                if (leftChar == rightChar) {
+                    for (int k = i; k < j; k++) result[k] = leftChar;
+                } else if (leftChar == 'R' && rightChar == 'L') {
+                    int l = i, r = j - 1;
+                    while (l < r) {
+                        result[l++] = 'R';
+                        result[r--] = 'L';
+                    }
+]                }
+
+                i = j;
             } else {
-                for (int i = 0; i < span / 2; ++i)
-                    res.append('R');
-                if (span % 2 == 1)
-                    res.append('.');
-                for (int i = 0; i < span / 2; ++i)
-                    res.append('L');
+                i++;
             }
-            prev = curr;
         }
-        return res.toString();
+
+        return new String(result);
     }
 }

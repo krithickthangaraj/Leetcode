e511b1c5c45ee0a1aa6c66ea6e896ce6f1f94667
KRITHICK THANGARAJ <krithick18112004@gmail.com>
2025-05-11 11:43:12 +0530

    Time: 0 ms (100%), Space: 42.1 MB (85.27%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.java
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.java
@@ -1,14 +1,3 @@
 class Solution {
-    public boolean threeConsecutiveOdds(int[] arr) {
-        int count = 0;
-        for (int x : arr) {
-            if ((x & 1) == 1) {
-                count++;
-                if (count == 3) return true;
-            } else {
-                count = 0;
-            }
-        }
-        return false;
-    }
+    public boolean threeConsecutiveOdds(int[] arr) { int c=0; for(int x:arr) if((c=(x&1)==1?c+1:0)==3) return true; return false; }
 }

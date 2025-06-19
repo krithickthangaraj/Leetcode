05ce97c31214cd12c945cf1abaabda1e840ced5d
KRITHICK THANGARAJ <krithick18112004@gmail.com>
2025-06-19 14:14:41 +0530

    Time: 0 ms (100%), Space: 42.6 MB (7.79%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void rotate(int[][] mat) {
+        int n=mat.length;
+        int m=mat[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=mat[i][j];
+                mat[i][j]=mat[j][i];
+                mat[j][i]=temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            
+                int l=0;
+                int r=m-1;
+                while(l<r){
+                    int temp=mat[i][l];
+                    mat[i][l]=mat[i][r];
+                    mat[i][r]=temp;
+                    l++;
+                    r--;
+                }
+            }
+        
+    }
+}

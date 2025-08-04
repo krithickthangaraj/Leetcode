39ea7ba7682d8537a2ee4374235c511ce433f724
KRITHICK THANGARAJ <krithick18112004@gmail.com>
2025-08-04 12:21:17 +0530

    Time: 7 ms (96.05%), Space: 57.1 MB (30.71%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPal(String s,int start,int end){
+        while(start<=end){
+            if(s.charAt(start++)!= s.charAt(end--)) return false;
+
+        }
+        return true;
+    }
+    public void solve(int index ,List<List<String>> res,List<String> in,String s){
+        if(index==s.length()) {
+            res.add(new ArrayList<>(in));
+            return ;
+        }
+
+        for(int i=index;i<s.length();i++){
+            if(isPal(s,index,i)){
+                in.add(s.substring(index,i+1));
+                solve(i+1,res,in,s);
+                in.remove(in.size()-1);
+            }
+        }
+        
+    }
+    public List<List<String>> partition(String s) {
+        List<List<String>> res= new ArrayList<>();
+        List<String> in=new ArrayList<>();
+        solve(0,res,in,s); 
+        return res;
+        
+    }
+}

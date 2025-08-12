cc246ee04759d5e5f8d5bf414bf3fe4813a4bb80
KRITHICK THANGARAJ <krithick18112004@gmail.com>
2025-08-12 19:55:33 +0530

    Time: 1 ms (98.37%), Space: 41.7 MB (20.64%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,40 +1,26 @@
+
 class Solution {
-    public void fun(List<List<Integer>> ans,List<Integer> in,boolean[] freq,int[] nums){
-        if(in.size()==nums.length){
-            ans.add(new ArrayList<>(in));
-            return;
-        }
-        for(int i=0;i<nums.length;i++){
-            if(!freq[i]){
-                freq[i]=true;
-                in.add(nums[i]);
-                fun(ans,in,freq,nums);
-                in.remove(in.size()-1);
-                freq[i]=false;
-            }
-        }
-    }
     public String getPermutation(int n, int k) {
-        int[] nums=new int[n];
-        for(int i=0;i<n;i++) nums[i]=i+1;
-        List<List<Integer>> ans =new ArrayList<>();
-        List<Integer> in=new ArrayList<>();
-        boolean freq[]= new boolean[nums.length];
-        fun(ans,in,freq,nums);
-        ans.sort((a, b) -> {
-            String sa = a.toString();
-            String sb = b.toString();
-            return sa.compareTo(sb);
-        });
+        List<Integer> numbers = new ArrayList<>();
+        int[] fact = new int[n + 1];
+        fact[0] = 1;
 
+        // Precompute factorials and number list
+        for (int i = 1; i <= n; i++) {
+            fact[i] = fact[i - 1] * i;
+            numbers.add(i);
+        }
+
+        k--; // convert to 0-based index
         StringBuilder sb = new StringBuilder();
-            for (int num : ans.get(k - 1)) {
-                sb.append(num);
-            }
-        return sb.toString();
 
-        
+        for (int i = n; i >= 1; i--) {
+            int index = k / fact[i - 1];
+            sb.append(numbers.get(index));
+            numbers.remove(index);
+            k %= fact[i - 1];
+        }
+
+        return sb.toString();
     }
 }
-
-

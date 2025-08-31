0ad155116d20f9c35c7f832c577412b90ae7471a
KRITHICK THANGARAJ <krithick18112004@gmail.com>
2025-08-31 12:24:31 +0530

    Time: 203 ms (66.42%), Space: 41.2 MB (56.8%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -6,12 +6,15 @@ class Solution {
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){       
                   if(board[i][j]=='.'){
+
                     for(char c='1';c<='9';c++){
+
                         if(isValid(board,i,j,c)){
                             board[i][j]=c;                         
                             if(solve(board)==true) return true;
                             else board[i][j]='.';
                         }
+                        
                     }
                     return false;
                 }
@@ -23,7 +26,7 @@ class Solution {
         for(int i=0;i<9;i++){
             if(board[row][i]==c || board[i][col]==c) return false;
         }
-        int curr_row=3 *( row/3 );
+        int curr_row=3 *(row/3 );
         int curr_col=3* (col/3);
         for(int i=curr_row;i<curr_row+3;i++){
             for(int j=curr_col;j<curr_col+3;j++){
